d6e7283d69908caf184dff7bbc5ef6160e83f91f
Dexaran <dexaran820@gmail.com>
2017-03-11 10:42:36 +0400

    Commented everything that is not required

--- ERC23_receiver_example.sol
+++ ERC23_receiver_example.sol
@@ -9,17 +9,15 @@ pragma solidity ^0.4.9;
  contract ContractReciever {
      
     //supported token contracts are stored here
-    mapping (address => bool) supportedTokens;
+    //mapping (address => bool) supportedTokens;
     
     /*
     //contract creator can add supported tokens
     function addToken(address _token){     //onlyOwner
         supportedTokens[_token]=true;
     }
-    */
     
     //if token is now disabled, replacced or not supported for other reasons we it can be disabled
-    /*
     function removeToken(address _token){  //onlyOwner
         supportedTokens[_token]=false;
     }
@@ -28,11 +26,13 @@ pragma solidity ^0.4.9;
     //Fallback fuction analogue called from token contract when token transaction to this contract appears
     function tokenFallback(address _from, uint _value, bytes _data){
     
-        if(supportedTokens[msg.sender])
-        {
+        //if(supportedTokens[msg.sender])
+        //{
             //on token transfer handler code here
-        }
+        //}
         //throw any accident transfers of not supported tokens
-        else{ throw; }
+        //else{ throw; }
+        
+        throw;
     }
  }
