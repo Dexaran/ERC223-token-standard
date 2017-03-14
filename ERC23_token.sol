2bcf59ad7d9e82287b54b88c6f8e66402584e4d5
Dexaran <dexaran820@gmail.com>
2017-03-14 19:58:56 +0400

    ERC23 interface update

--- ERC23_token.sol
+++ ERC23_token.sol
@@ -7,6 +7,7 @@ contract ERC23 {
   function balanceOf(address who) constant returns (uint);
   function allowance(address owner, address spender) constant returns (uint);
 
+  function transfer(address to, uint value) returns (bool ok);
   function transfer(address to, uint value, bytes data) returns (bool ok);
   function transferFrom(address from, address to, uint value) returns (bool ok);
   function approve(address spender, uint value) returns (bool ok);
