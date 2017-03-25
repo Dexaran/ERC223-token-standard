691319ebb5da6e8f10359b0bf146bf95eeded56f
Dexaran <dexaran820@gmail.com>
2017-03-25 21:06:58 +0400

    getSig and TKN declaration changed

--- contracts/implementation/StandardReceiver.sol
+++ contracts/implementation/StandardReceiver.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.4.8;
 import "../interface/ERC23Receiver.sol";
 
 contract StandardReceiver is ERC23Receiver {
-  Tkn tkn;
+
+//We don't need to declare TKN object here.
 
   struct Tkn {
     address addr;
@@ -17,25 +18,22 @@ contract StandardReceiver is ERC23Receiver {
   }
 
   function tokenFallback(address _sender, address _origin, uint _value, bytes _data) returns (bool ok) {
+    //Just declare it here in a local function to make it safe accessing .tkn values
+    TKN tkn;
+    
     if (!supportsToken(msg.sender)) return false;
-
-    // Problem: This will do a sstore which is expensive gas wise. Find a way to keep it in memory.
     tkn = Tkn(msg.sender, _sender, _origin, _value, _data, getSig(_data));
-    __isTokenFallback = true;
     if (!address(this).delegatecall(_data)) return false;
 
     // avoid doing an overwrite to .token, which would be more expensive
-    // makes accessing .tkn values outside tokenPayable functions unsafe
-    __isTokenFallback = false;
+    delete tkn;
 
     return true;
   }
 
   function getSig(bytes _data) private returns (bytes4 sig) {
-    uint l = _data.length < 4 ? _data.length : 4;
-    for (uint i = 0; i < l; i++) {
-      sig = bytes4(uint(sig) + uint(_data[i]) * (2 ** (8 * (l - 1 - i))));
-    }
+    uint32 u = uint32(_data[3]) + (uint32(_data[2]) << 8) + (uint32(_data[1]) << 16) + (uint32(_data[0]) << 24);
+    bytes4 sig = bytes4(u);
   }
 
   bool __isTokenFallback;
