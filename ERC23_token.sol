6bb198555640f88f17de15a290172d5bb8845faf
Dexaran <dexaran820@gmail.com>
2017-05-03 20:42:51 +0400

    Update ERC23_token.sol

--- ERC23_token.sol
+++ ERC23_token.sol
@@ -1,18 +1,22 @@
 pragma solidity ^0.4.9;
  
+ contract ContractReceiver {
+    function tokenFallback(address _from, uint _value, bytes _data){}
+}
+ 
  /* New ERC23 contract interface */
 
 contract ERC23 {
   uint public totalSupply;
   function balanceOf(address who) constant returns (uint);
-  function allowance(address owner, address spender) constant returns (uint);
+  
+  function name() constant returns (string _name);
+  function symbol() constant returns (string _symbol);
+  function decimals() constant returns (uint8 _decimals);
 
   function transfer(address to, uint value) returns (bool ok);
   function transfer(address to, uint value, bytes data) returns (bool ok);
-  function transferFrom(address from, address to, uint value) returns (bool ok);
-  function approve(address spender, uint value) returns (bool ok);
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
+  event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
 }
 
 
@@ -25,93 +29,84 @@ contract ERC23 {
 contract ERC23Token is ERC23 {
 
   mapping(address => uint) balances;
-  mapping (address => mapping (address => uint)) allowed;
+  
+  string public name;
+  string public symbol;
+  uint8 public decimals;
+  
+  
+  //function to access name of token
+  function name() constant returns (string _name) {
+      return name;
+  }
+  //function to access symbol of token
+  function symbol() constant returns (string _symbol) {
+      return symbol;
+  }
+  //function to access decimals of token
+  function decimals() constant returns (uint8 _decimals) {
+      return decimals;
+  }
+  
+  
 
   //function that is called when a user or another contract wants to transfer funds
   function transfer(address _to, uint _value, bytes _data) returns (bool success) {
   
-    //filtering if the target is a contract with bytecode inside it
-    if(isContract(_to)) {
-        transferToContract(_to, _value, _data);
+    //standard function transfer similar to ERC20 transfer with no _data
+    //added due to backwards compatibility reasons
+    uint codeLength;
+    
+    assembly {
+        //retrieve the size of the code on target address, this needs assembly
+        codeLength := extcodesize(_to)
+    }
+    
+    if(codeLength>0) {
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        ContractReceiver reciever = ContractReceiver(_to);
+        reciever.tokenFallback(msg.sender, _value, _data);
+        Transfer(msg.sender, _to, _value, _data);
+        return true;
     }
     else {
-        transferToAddress(_to, _value, _data);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value, _data);
+        return true;
     }
-    return true;
   }
   
   function transfer(address _to, uint _value) returns (bool success) {
       
     //standard function transfer similar to ERC20 transfer with no _data
     //added due to backwards compatibility reasons
-    bytes emptyData;
-    if(isContract(_to)) {
-        transferToContract(_to, _value, emptyData);
+    bytes memory emptyData;
+    uint codeLength;
+    
+    assembly {
+        //retrieve the size of the code on target address, this needs assembly
+        codeLength := extcodesize(_to)
+    }
+    
+    if(codeLength>0) {
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        ContractReceiver reciever = ContractReceiver(_to);
+        reciever.tokenFallback(msg.sender, _value, emptyData);
+        Transfer(msg.sender, _to, _value, emptyData);
+        return true;
     }
     else {
-        transferToAddress(_to, _value, emptyData);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value, emptyData);
+        return true;
     }
-    return true;
-  }
-
-  //function that is called when transaction target is an address
-  function transferToAddress(address _to, uint _value, bytes _data) returns (bool success) {
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-  
-  //function that is called when transaction target is a contract
-  function transferToContract(address _to, uint _value, bytes _data) returns (bool success) {
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    contractReceiver reciever = contractReceiver(_to);
-    reciever.tokenFallback(msg.sender, _value, _data);
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-  
-  //assemble the given address bytecode. If bytecode exists then the _addr is a contract.
-  function isContract(address _addr) private returns (bool is_contract) {
-      uint length;
-      assembly {
-            //retrieve the size of the code on target address, this needs assembly
-            length := extcodesize(_addr)
-        }
-        if(length>0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-  function transferFrom(address _from, address _to, uint _value) returns (bool success) {
-    var _allowance = allowed[_from][msg.sender];
-    
-    if(_value > _allowance) {
-        throw;
-    }
-
-    balances[_to] += _value;
-    balances[_from] -= _value;
-    allowed[_from][msg.sender] -= _value;
-    Transfer(_from, _to, _value);
-    return true;
   }
 
   function balanceOf(address _owner) constant returns (uint balance) {
     return balances[_owner];
-  }
-
-  function approve(address _spender, uint _value) returns (bool success) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) constant returns (uint remaining) {
-    return allowed[_owner][_spender];
   }
 }
