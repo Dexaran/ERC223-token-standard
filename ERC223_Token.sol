c5877cbff987c743a9ccf5cbcf0aa87113645a9e
Dexaran <dexaran820@gmail.com>
2017-05-08 22:01:48 +0400

    Update and rename ERC23_token.sol to ERC223_Token.sol

--- ERC223_Token.sol
+++ ERC223_Token.sol
@@ -1,20 +1,7 @@
 pragma solidity ^0.4.9;
- 
- /* New ERC23 contract interface */
 
-contract ERC23 {
-  uint public totalSupply;
-  function balanceOf(address who) constant returns (uint);
-  
-  function name() constant returns (string _name);
-  function symbol() constant returns (string _symbol);
-  function decimals() constant returns (uint8 _decimals);
-
-  function transfer(address to, uint value) returns (bool ok);
-  function transfer(address to, uint value, bytes data) returns (bool ok);
-  event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
-}
-
+import "./Receiver_Interface.sol";
+import "./ERC223_Interface.sol";
 
  /**
  * ERC23 token by Dexaran
@@ -22,7 +9,7 @@ contract ERC23 {
  * https://github.com/Dexaran/ERC23-tokens
  */
  
-contract ERC23Token is ERC23 {
+contract ERC223Token is ERC223 {
 
   mapping(address => uint) balances;
   
@@ -31,30 +18,30 @@ contract ERC23Token is ERC23 {
   uint8 public decimals;
   
   
-  //function to access name of token
+  // Function to access name of token .
   function name() constant returns (string _name) {
       return name;
   }
-  //function to access symbol of token
+  // Function to access symbol of token .
   function symbol() constant returns (string _symbol) {
       return symbol;
   }
-  //function to access decimals of token
+  // Function to access decimals of token .
   function decimals() constant returns (uint8 _decimals) {
       return decimals;
   }
   
   
 
-  //function that is called when a user or another contract wants to transfer funds
+  // Function that is called when a user or another contract wants to transfer funds .
   function transfer(address _to, uint _value, bytes _data) returns (bool success) {
   
-    //standard function transfer similar to ERC20 transfer with no _data
-    //added due to backwards compatibility reasons
+    // Standard function transfer similar to ERC20 transfer with no _data .
+    // Added due to backwards compatibility reasons .
     uint codeLength;
     
     assembly {
-        //retrieve the size of the code on target address, this needs assembly
+        // Retrieve the size of the code on target address, this needs assembly .
         codeLength := extcodesize(_to)
     }
     
@@ -73,15 +60,15 @@ contract ERC23Token is ERC23 {
     return true;
   }
   
+  // Standard function transfer similar to ERC20 transfer with no _data .
+  // Added due to backwards compatibility reasons .
   function transfer(address _to, uint _value) returns (bool success) {
       
-    //standard function transfer similar to ERC20 transfer with no _data
-    //added due to backwards compatibility reasons
-    bytes memory emptyData;
+    bytes memory _empty;
     uint codeLength;
     
     assembly {
-        //retrieve the size of the code on target address, this needs assembly
+        // Retrieve the size of the code on target address, this needs assembly .
         codeLength := extcodesize(_to)
     }
     
@@ -89,19 +76,18 @@ contract ERC23Token is ERC23 {
         balances[msg.sender] -= _value;
         balances[_to] += _value;
         ContractReceiver reciever = ContractReceiver(_to);
-        reciever.tokenFallback(msg.sender, _value, emptyData);
-        Transfer(msg.sender, _to, _value, emptyData);
-        return true;
+        reciever.tokenFallback(msg.sender, _value, _empty);
+        Transfer(msg.sender, _to, _value, _empty);
     }
     else {
         balances[msg.sender] -= _value;
         balances[_to] += _value;
-        Transfer(msg.sender, _to, _value, emptyData);
-        return true;
+        Transfer(msg.sender, _to, _value, _empty);
     }
+    return true;
   }
 
   function balanceOf(address _owner) constant returns (uint balance) {
     return balances[_owner];
   }
-}
+} 
