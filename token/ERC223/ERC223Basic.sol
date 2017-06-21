a45059348e9e9e8b5adc3ac3d33256077b8b9f17
Prophet Daniel <prophetdaniel@ethereumclassic.org>
2017-06-21 20:34:40 -0300

    Update ERC223Basic.sol
    Fixed error 53 bits according to https://github.com/trufflesuite/truffle/issues/405.

--- token/ERC223/ERC223Basic.sol
+++ token/ERC223/ERC223Basic.sol
@@ -4,5 +4,5 @@ import '../ERC20Basic.sol';
 
 contract ERC223Basic is ERC20Basic {
     function transfer(address to, uint value, bytes data);
-    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
 }
