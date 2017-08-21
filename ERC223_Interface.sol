20f9baaa34dce1b02b38e88247be4c6426d13d0b
Dexaran <dexaran820@gmail.com>
2017-08-21 16:14:08 -0400

    added custom_fallback implementation.

--- ERC223_Interface.sol
+++ ERC223_Interface.sol
@@ -13,5 +13,6 @@ contract ERC223 {
 
   function transfer(address to, uint value) returns (bool ok);
   function transfer(address to, uint value, bytes data) returns (bool ok);
+  function transfer(address to, uint value, bytes data, string custom_fallback) returns (bool ok);
   event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
 }
