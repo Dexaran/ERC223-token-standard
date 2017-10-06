80fa107b2343ca400b7b222e8d48b11dfb205509
Dexaran <dexaran820@gmail.com>
2017-10-06 08:49:14 +0100

    Update ERC223_token.sol

--- token/ERC223/ERC223_token.sol
+++ token/ERC223/ERC223_token.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
-import './ERC223Basic.sol';
-import './ERC223ReceivingContract.sol';
+import './ERC223_interface.sol';
+import './ERC223_receiving_contract.sol';
 import '../../SafeMath.sol';
 
 contract ERC223BasicToken is ERC223Basic{
