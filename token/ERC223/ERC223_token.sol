2d664f038554514d417fb36e0985f6a238f93fac
Dexaran <dexaran820@gmail.com>
2017-10-12 19:41:57 +0100

    Comment improvements.

--- token/ERC223/ERC223_token.sol
+++ token/ERC223/ERC223_token.sol
@@ -2,53 +2,80 @@ pragma solidity ^0.4.11;
 
 import './ERC223_interface.sol';
 import './ERC223_receiving_contract.sol';
-import '../../SafeMath.sol';
+import '././SafeMath.sol';
 
-contract ERC223BasicToken is ERC223Interface{
+/**
+ * @title ERC223 standard token implementation.
+ */
+contract ERC223BasicToken is ERC223Interface {
     using SafeMath for uint;
 
-    mapping(address => uint) balances;
-
-    // Function that is called when a user or another contract wants to transfer funds .
-    function transfer(address to, uint value, bytes data) {
+    mapping(address => uint) balances; // List of user balances.
+    
+    /**
+     * @dev Transfer the specified amount of tokens to the specified address.
+     *      Invokes the `tokenFallback` function if the recipient is a contract.
+     *      The token transfer fails if the recipient is a contract
+     *      but does not implement the `tokenFallback` function
+     *      or the fallback function to receive funds.
+     *
+     * @param _to    Receiver address.
+     * @param _value Amount of tokens that will be transferred.
+     * @param _data  Transaction metadata.
+     */
+    function transfer(address _to, uint _value, bytes _data) {
         // Standard function transfer similar to ERC20 transfer with no _data .
         // Added due to backwards compatibility reasons .
         uint codeLength;
 
         assembly {
             // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(to)
+            codeLength := extcodesize(_to)
         }
 
-        balances[msg.sender] = balances[msg.sender].sub(value);
-        balances[to] = balances[to].add(value);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
         if(codeLength>0) {
-            ERC223ReceivingContract receiver = ERC223ReceivingContract(to);
-            receiver.tokenFallback(msg.sender, value, data);
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, _data);
         }
-        Transfer(msg.sender, to, value, data);
+        Transfer(msg.sender, _to, _value, _data);
     }
-
-    // Standard function transfer similar to ERC20 transfer with no _data .
-    // Added due to backwards compatibility reasons .
-    function transfer(address to, uint value) {
+    
+    /**
+     * @dev Transfer the specified amount of tokens to the specified address.
+     *      This function works the same with the previous one
+     *      but doesn't contain `_data` param.
+     *      Added due to backwards compatibility reasons.
+     *
+     * @param _to    Receiver address.
+     * @param _value Amount of tokens that will be transferred.
+     */
+    function transfer(address _to, uint _value) {
         uint codeLength;
         bytes memory empty;
 
         assembly {
             // Retrieve the size of the code on target address, this needs assembly .
-            codeLength := extcodesize(to)
+            codeLength := extcodesize(_to)
         }
 
-        balances[msg.sender] = balances[msg.sender].sub(value);
-        balances[to] = balances[to].add(value);
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
         if(codeLength>0) {
-            ERC223ReceivingContract receiver = ERC223ReceivingContract(to);
-            receiver.tokenFallback(msg.sender, value, empty);
+            ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+            receiver.tokenFallback(msg.sender, _value, empty);
         }
-        Transfer(msg.sender, to, value, empty);
+        Transfer(msg.sender, _to, _value, empty);
     }
 
+    
+    /**
+     * @dev Returns balance of the `_owner`.
+     *
+     * @param _owner   The address whose balance will be returned.
+     * @return balance Balance of the `_owner`.
+     */
     function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
     }
