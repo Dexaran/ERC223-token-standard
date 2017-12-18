cb0fe2b776e670a301c1ecf1f18ee1fc74caf6de
Zoe Nolan <github@zoenolan.org>
2017-12-18 06:54:57 +0000

    Add public modifers to token contracts to fix compiler warnings

--- token/ERC223/ERC223_interface.sol
+++ token/ERC223/ERC223_interface.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.11;
 
 contract ERC223Interface {
     uint public totalSupply;
-    function balanceOf(address who) constant returns (uint);
-    function transfer(address to, uint value);
-    function transfer(address to, uint value, bytes data);
+    function balanceOf(address who) public constant returns (uint);
+    function transfer(address to, uint value) public;
+    function transfer(address to, uint value, bytes data) public;
     event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
 }
--- token/ERC223/ERC223_receiving_contract.sol
+++ token/ERC223/ERC223_receiving_contract.sol
@@ -12,5 +12,5 @@ contract ERC223ReceivingContract {
  * @param _value Amount of tokens.
  * @param _data  Transaction metadata.
  */
-    function tokenFallback(address _from, uint _value, bytes _data);
+    function tokenFallback(address _from, uint _value, bytes _data) public;
 }
