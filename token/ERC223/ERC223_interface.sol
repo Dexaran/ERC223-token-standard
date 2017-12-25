4b2159177da5b66b92d2969551fcc827bb162418
Dexaran <dexaran820@gmail.com>
2017-12-25 07:20:23 +0000

    Update ERC223_interface.sol

--- token/ERC223/ERC223_interface.sol
+++ token/ERC223/ERC223_interface.sol
@@ -5,5 +5,5 @@ contract ERC223Interface {
     function balanceOf(address who) constant returns (uint);
     function transfer(address to, uint value);
     function transfer(address to, uint value, bytes data);
-    event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
 }
