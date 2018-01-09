0427f5086572ccb4dffdf73a7a691a63d1d11c69
Zoe Nolan <github@zoenolan.org>
2018-01-09 23:27:53 +0000

    Fix compiler warnings

--- ERC223_Token.sol
+++ ERC223_Token.sol
@@ -15,19 +15,19 @@ contract SafeMath {
     uint256 constant public MAX_UINT256 =
     0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
-    function safeAdd(uint256 x, uint256 y) constant internal returns (uint256 z) {
-        if (x > MAX_UINT256 - y) throw;
+    function safeAdd(uint256 x, uint256 y) pure internal returns (uint256 z) {
+        if (x > MAX_UINT256 - y) revert();
         return x + y;
     }
 
-    function safeSub(uint256 x, uint256 y) constant internal returns (uint256 z) {
-        if (x < y) throw;
+    function safeSub(uint256 x, uint256 y) pure internal returns (uint256 z) {
+        if (x < y) revert();
         return x - y;
     }
 
-    function safeMul(uint256 x, uint256 y) constant internal returns (uint256 z) {
+    function safeMul(uint256 x, uint256 y) pure internal returns (uint256 z) {
         if (y == 0) return 0;
-        if (x > MAX_UINT256 / y) throw;
+        if (x > MAX_UINT256 / y) revert();
         return x * y;
     }
 }
@@ -43,31 +43,31 @@ contract ERC223Token is ERC223, SafeMath {
   
   
   // Function to access name of token .
-  function name() constant returns (string _name) {
+  function name() public view returns (string _name) {
       return name;
   }
   // Function to access symbol of token .
-  function symbol() constant returns (string _symbol) {
+  function symbol() public view returns (string _symbol) {
       return symbol;
   }
   // Function to access decimals of token .
-  function decimals() constant returns (uint8 _decimals) {
+  function decimals() public view returns (uint8 _decimals) {
       return decimals;
   }
   // Function to access total supply of tokens .
-  function totalSupply() constant returns (uint256 _totalSupply) {
+  function totalSupply() public view returns (uint256 _totalSupply) {
       return totalSupply;
   }
   
   
   // Function that is called when a user or another contract wants to transfer funds .
-  function transfer(address _to, uint _value, bytes _data, string _custom_fallback) returns (bool success) {
+  function transfer(address _to, uint _value, bytes _data, string _custom_fallback) public returns (bool success) {
       
     if(isContract(_to)) {
-        if (balanceOf(msg.sender) < _value) throw;
+        if (balanceOf(msg.sender) < _value) revert();
         balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
         balances[_to] = safeAdd(balanceOf(_to), _value);
-        assert(_to.call.value(0)(bytes4(sha3(_custom_fallback)), msg.sender, _value, _data));
+        assert(_to.call.value(0)(bytes4(keccak256(_custom_fallback)), msg.sender, _value, _data));
         Transfer(msg.sender, _to, _value, _data);
         return true;
     }
@@ -78,7 +78,7 @@ contract ERC223Token is ERC223, SafeMath {
   
 
   // Function that is called when a user or another contract wants to transfer funds .
-  function transfer(address _to, uint _value, bytes _data) returns (bool success) {
+  function transfer(address _to, uint _value, bytes _data) public returns (bool success) {
       
     if(isContract(_to)) {
         return transferToContract(_to, _value, _data);
@@ -90,7 +90,7 @@ contract ERC223Token is ERC223, SafeMath {
   
   // Standard function transfer similar to ERC20 transfer with no _data .
   // Added due to backwards compatibility reasons .
-  function transfer(address _to, uint _value) returns (bool success) {
+  function transfer(address _to, uint _value) public returns (bool success) {
       
     //standard function transfer similar to ERC20 transfer with no _data
     //added due to backwards compatibility reasons
@@ -104,7 +104,7 @@ contract ERC223Token is ERC223, SafeMath {
 }
 
 //assemble the given address bytecode. If bytecode exists then the _addr is a contract.
-  function isContract(address _addr) private returns (bool is_contract) {
+  function isContract(address _addr) private view returns (bool is_contract) {
       uint length;
       assembly {
             //retrieve the size of the code on target address, this needs assembly
@@ -115,7 +115,7 @@ contract ERC223Token is ERC223, SafeMath {
 
   //function that is called when transaction target is an address
   function transferToAddress(address _to, uint _value, bytes _data) private returns (bool success) {
-    if (balanceOf(msg.sender) < _value) throw;
+    if (balanceOf(msg.sender) < _value) revert();
     balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
     balances[_to] = safeAdd(balanceOf(_to), _value);
     Transfer(msg.sender, _to, _value, _data);
@@ -124,7 +124,7 @@ contract ERC223Token is ERC223, SafeMath {
   
   //function that is called when transaction target is a contract
   function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
-    if (balanceOf(msg.sender) < _value) throw;
+    if (balanceOf(msg.sender) < _value) revert();
     balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
     balances[_to] = safeAdd(balanceOf(_to), _value);
     ContractReceiver receiver = ContractReceiver(_to);
@@ -134,7 +134,7 @@ contract ERC223Token is ERC223, SafeMath {
 }
 
 
-  function balanceOf(address _owner) constant returns (uint balance) {
+  function balanceOf(address _owner) public view returns (uint balance) {
     return balances[_owner];
   }
 }
