2040b117522b61530d142d6ef7138e16b016c7f6
Zoe Nolan <github@zoenolan.org>
2018-04-27 20:43:12 +0100

    Update to the emit syntax for events

--- ERC223_Token.sol
+++ ERC223_Token.sol
@@ -68,7 +68,7 @@ contract ERC223Token is ERC223, SafeMath {
         balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
         balances[_to] = safeAdd(balanceOf(_to), _value);
         assert(_to.call.value(0)(bytes4(keccak256(_custom_fallback)), msg.sender, _value, _data));
-        Transfer(msg.sender, _to, _value, _data);
+        emit Transfer(msg.sender, _to, _value, _data);
         return true;
     }
     else {
@@ -118,7 +118,7 @@ contract ERC223Token is ERC223, SafeMath {
     if (balanceOf(msg.sender) < _value) revert();
     balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
     balances[_to] = safeAdd(balanceOf(_to), _value);
-    Transfer(msg.sender, _to, _value, _data);
+    emit Transfer(msg.sender, _to, _value, _data);
     return true;
   }
   
@@ -129,7 +129,7 @@ contract ERC223Token is ERC223, SafeMath {
     balances[_to] = safeAdd(balanceOf(_to), _value);
     ContractReceiver receiver = ContractReceiver(_to);
     receiver.tokenFallback(msg.sender, _value, _data);
-    Transfer(msg.sender, _to, _value, _data);
+    emit Transfer(msg.sender, _to, _value, _data);
     return true;
 }
 
