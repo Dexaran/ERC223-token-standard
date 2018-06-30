b74e6867411dff8a90fd215069a1e1e78ef6daf5
Zoe Nolan <github@zoenolan.org>
2018-06-30 09:45:04 +0100

    Add cast to remove compiler warning

--- ERC223_Token.sol
+++ ERC223_Token.sol
@@ -67,7 +67,7 @@ contract ERC223Token is ERC223, SafeMath {
         if (balanceOf(msg.sender) < _value) revert();
         balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
         balances[_to] = safeAdd(balanceOf(_to), _value);
-        assert(_to.call.value(0)(bytes4(keccak256(_custom_fallback)), msg.sender, _value, _data));
+        assert(_to.call.value(0)(bytes4(keccak256(bytes(_custom_fallback))), msg.sender, _value, _data));
         emit Transfer(msg.sender, _to, _value, _data);
         return true;
     }
