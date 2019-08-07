f3e097c90367511b09e3a85a6930fbbf38d0c60b
Dexaran <dexaran820@gmail.com>
2019-08-07 18:32:12 +0400

    Removing old files

--- token/ERC223/ERC223_interface.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract ERC223Interface {
-    uint public totalSupply;
-    function balanceOf(address who) public view returns (uint);
-    function transfer(address to, uint value) public returns (bool success);
-    function transfer(address to, uint value, bytes memory data) public returns (bool success);
-    event Transfer(address indexed from, address indexed to, uint value, bytes data);
-}
