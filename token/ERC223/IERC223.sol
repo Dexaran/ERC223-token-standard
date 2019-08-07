cb97bc22ec0d78785da0af39b60e519c5c1b943c
Dexaran <dexaran820@gmail.com>
2019-08-07 17:31:53 +0300

    Added modular ERC223 token contracts.

---
+++ token/ERC223/IERC223.sol
@@ -0,0 +1,41 @@
+    
+pragma solidity ^0.5.0;
+
+/**
+ * @dev Interface of the ERC777Token standard as defined in the EIP.
+ *
+ * This contract uses the
+ * [ERC1820 registry standard](https://eips.ethereum.org/EIPS/eip-1820) to let
+ * token holders and recipients react to token movements by using setting implementers
+ * for the associated interfaces in said registry. See `IERC1820Registry` and
+ * `ERC1820Implementer`.
+ */
+
+contract IERC223 {
+    /**
+     * @dev Returns the total supply of the token.
+     */
+    uint public totalSupply;
+    
+    /**
+     * @dev Returns the balance of the `who` address.
+     */
+    function balanceOf(address who) public view returns (uint);
+        
+    /**
+     * @dev Transfers `value` tokens from `msg.sender` to `to` address
+     * and returns `true` on success.
+     */
+    function transfer(address to, uint value) public returns (bool success);
+        
+    /**
+     * @dev Transfers `value` tokens from `msg.sender` to `to` address with `data` parameter
+     * and returns `true` on success.
+     */
+    function transfer(address to, uint value, bytes memory data) public returns (bool success)
+     
+     /**
+     * @dev Event that is fired on successful transfer.
+     */
+    event Transfer(address indexed from, address indexed to, uint value, bytes data);
+}
---
+++ token/ERC223/README.md
@@ -0,0 +1,16 @@
+---
+sections:
+  - title: Core
+    contracts:
+      - IERC223
+      - ERC223
+  - title: Hooks
+    contracts:
+      - IERC223Recipient
+---
+
+This set of interfaces and contracts are all related to the [ERC223 token standard](https://github.com/ethereum/EIPs/issues/223).
+
+The token behavior itself is implemented in the core contracts: `IERC223`, `ERC223`.
+
+Additionally there are interfaces used to develop contracts that react to token movements: `IERC223Recipient`.
