03336a090724719c09cf4885a9e2c57fc835f280
Dexaran <dexaran820@gmail.com>
2021-09-17 18:41:50 +0400

    Updates for 0.8.0 solidity

--- token/ERC223/IERC223Recipient.sol
+++ token/ERC223/IERC223Recipient.sol
@@ -4,7 +4,17 @@ pragma solidity ^0.8.0;
  * @title Contract that will work with ERC223 tokens.
  */
  
-contract IERC223Recipient { 
+abstract contract IERC223Recipient {
+
+
+ struct ERC223TransferInfo
+    {
+        address token_contract;
+        address sender;
+        uint256 value;
+        bytes   data;
+    }
+    
 /**
  * @dev Standard ERC223 function that will handle incoming token transfers.
  *
@@ -12,5 +22,21 @@ contract IERC223Recipient {
  * @param _value Amount of tokens.
  * @param _data  Transaction metadata.
  */
-    function tokenReceived(address _from, uint _value, bytes memory _data) public;
+    function tokenReceived(address _from, uint _value, bytes memory _data) public virtual
+    {
+        /**
+         * @dev Note that inside of the token transaction handler the actual sender of token transfer is accessible via the tkn.sender variable
+         * (analogue of msg.sender for Ether transfers)
+         * 
+         * tkn.value - is the amount of transferred tokens
+         * tkn.data  - is the "metadata" of token transfer
+         * tkn.token_contract is most likely equal to msg.sender because the token contract typically invokes this function
+        */
+        tkn.token_contract = msg.sender;
+        tkn.sender         = _sender;
+        tkn.value          = _value;
+        tkn.data           = _data;
+        
+        // ACTUAL CODE
+    }
 }
